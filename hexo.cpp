10d10236e316b924d7561b067fd6b9875ad37a89
Wang Fan <ppdg@foxmail.com>
2015-07-07 16:00:38 +0800

    post

--- hexo.cpp
+++ hexo.cpp
@@ -20,7 +20,7 @@ int main(int argc, char * argv[])
 	sprintf(fn,"%d-%d-%d-%d-%d-%d.md",1900+p->tm_year,1+p->tm_mon,p->tm_mday,p->tm_hour,p->tm_min,p->tm_sec);
 	FILE * fp;
 	fp = fopen(fn,"w");
-	fprintf(fp,"title: []\ndate: %d-%d-%d %d:%d:%d\ntags: \n---\n",1900+p->tm_year,1+p->tm_mon,p->tm_mday,p->tm_hour,p->tm_min,p->tm_sec);
+	fprintf(fp,"title: []\ndate: %d-%d-%d %d:%d:%d\ntags: \n---\n<!--more-->\n",1900+p->tm_year,1+p->tm_mon,p->tm_mday,p->tm_hour,p->tm_min,p->tm_sec);
 	fclose(fp);
 	sprintf(cmd,"haroopad %s",fn);
 	system(cmd);
