c10392e32f32e77791d071631db77bcfba64de98
Wang Fan <ppdg@foxmail.com>
2015-07-25 04:05:52 +0800

    post

--- dp.sh
+++ dp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "s#http://i.imgur.com#http://i.wangfan.net:2048#g" *.md
+sed -i "s#http://i.imgur.com#http://ppdg.ml#g" *.md
 git add .
 git commit -m "post"
 git push origin master
