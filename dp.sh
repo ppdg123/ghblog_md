ff51572d7767eec452f7885af7697e4b67534539
Fan Wang <ppdg@foxmail.com>
2015-08-17 23:53:07 +0800

    post

--- dp.sh
+++ dp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i "s#http://i.imgur.com#http://wangfan.net:9000#g" *.md
-sed -i "s#https://www.youtube.com#http://wangfan.net:9002#g" *.md
+sed -i "s#https://www.youtube.com/embed#http://wangfan.net:9002#g" *.md
 git add .
 git commit -m "post"
 git push origin master
