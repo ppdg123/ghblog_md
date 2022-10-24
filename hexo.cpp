e176b660377e8e817b467ed8c236c2250e9fc322
root <root@localhost.localdomain>
2022-10-24 21:10:30 +0800

    mod

--- hexo.cpp
+++ hexo.cpp
@@ -10,7 +10,7 @@ int main(int argc, char * argv[])
 	if(argc == 2)
 	{
 	    sprintf(cmd,"c9 open %s",argv[1]);
-	    system(cmd);
+	    //system(cmd);
 	    return 0;
 	}
 	time_t timep;
@@ -22,7 +22,7 @@ int main(int argc, char * argv[])
 	fp = fopen(fn,"w");
 	fprintf(fp,"title: []\ndate: %d-%d-%d %d:%d:%d\ntags: \n---\n<!--more-->\n",1900+p->tm_year,1+p->tm_mon,p->tm_mday,p->tm_hour,p->tm_min,p->tm_sec);
 	fclose(fp);
-	sprintf(cmd,"c9 open %s",fn);
-	system(cmd);
+	//sprintf(cmd,"c9 open %s",fn);
+	//system(cmd);
 return 0;
 }
